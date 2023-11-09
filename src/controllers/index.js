334878fa467e5df84f0860275e38284900fce3e9
avoidTheLite <metalsupremacist@gmail.com>
2023-11-09 10:26:00 -0500

    installed @types/express

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import { controllerFunction1 } from "./lib/controllerFunction1.js"
-import { controllerFunction2 } from "./lib/controllerFunction2.js/index.js";
-
-export default {
-    controllerFunction1,
-    controllerFunction2
-};
