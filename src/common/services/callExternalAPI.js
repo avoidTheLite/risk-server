5768d8c5f2cad873252550d2c454f27fe1ee5cdd
avoidTheLite <metalsupremacist@gmail.com>
2024-09-14 11:26:19 -0400

    cleanup external API calling function

--- src/common/services/callExternalAPI.js
+++ src/common/services/callExternalAPI.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
 
-export async function callExternalAPI(messages) {
-    const API_KEY = process.env.OPEN_AI_API_KEY
-    const API_URL = process.env.OPEN_AI_API_URL
+export async function getExternalAPI(body) {
+    // const API_KEY = process.env.API_KEY
+    const API_URL = process.env.SERVER_URL 
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`
+        // 'Authorization': `Bearer ${API_KEY}`
       };
       
-      const data = {
-        option1: 'option',
-        messages: messages,
-        number:50
-    };
+      ;
     try {
-        await axios.post(API_URL, data, {headers})
+        await axios.get(API_URL, body, {headers})
         .then(function(response){
             result = response.item;
         return result    
