312181016c94a7c9d1ecdde5f77f26975f750123
Justin Laptop <metalsupremacist@gmail.com>
2024-10-04 17:32:10 -0400

    fetches game state and renders it (poorly)

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,37 @@ function getGameState()
 {
     const state = "5"
     console.log('function = getGameState')
+    // Define the API URL
+const apiUrl = 'http://localhost:3001/game/2P2soc4vVB';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    document.getElementById('active-player').textContent = `Active Player = ${data.activePlayerId}`;
+    document.getElementById('turn-number').textContent = `Turn = ${data.turn}`;
+    document.getElementById('reinforcement-count').textContent = `Your Reinforcement Count = ${data.players[0].armies}`;
+    
+    let countriesDiv = document.getElementById('countries');
+    countriesDiv.textContent = ``;
+
+    data.country.forEach(country => {
+        let countryInfo = `${country.name}: Armies = ${country.armies}`;
+        let countryElement = document.createElement('div'); // Create a new div for each country
+        countryElement.textContent = countryInfo;
+        countryElement.classList.add('country-box')
+        countriesDiv.appendChild(countryElement); // Append the country div to the countries div
+    })
+})
+  .catch(error => {
+    console.error('Error:', error);
+  });
     return state
 }
 
